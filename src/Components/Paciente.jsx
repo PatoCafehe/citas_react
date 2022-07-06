dfc77b59970527f62e7a547b36896eb386f9a4a0
Patricio Cafehe <cafehepatricio@gmail.com>
2022-07-06 09:22:54 -0300

    first commit

---
+++ src/Components/Paciente.jsx
@@ -0,0 +1,62 @@
+const Paciente = ({ paciente, setPaciente, eliminarPaciente }) => {
+	const { nombre, propietario, email, fecha, sintomas, id } = paciente;
+
+	const handleEliminar = () => {
+		const respuesta = confirm('Deseas eliminar este paciente', id);
+
+		if (respuesta) {
+			eliminarPaciente(id);
+		}
+	};
+	return (
+		<div>
+			<div className="mx-5 bg-white shadow-md px-5 py-5 rounded-xl mb-3">
+				<p className="font-bold mt-3 mb-3 text-gray-700 uppercase">
+					Nombre: {''}
+					<span className="font-normal normal-case ">{nombre}</span>
+				</p>
+
+				<p className="font-bold mb-3 text-gray-700 uppercase">
+					Propietario: {''}
+					<span className="font-normal normal-case ">{propietario}</span>
+				</p>
+
+				<p className="font-bold mb-3 text-gray-700 uppercase">
+					Email: {''}
+					<span className="font-normal normal-case ">{email}</span>
+				</p>
+
+				<p className="font-bold mb-3 text-gray-700 uppercase">
+					Fecha Alta: {''}
+					<span className="font-normal normal-case ">{fecha}</span>
+				</p>
+
+				<p className="font-bold mb-5 text-gray-700 uppercase">
+					Síntomas: {''}
+					<span className="font-normal normal-case ">{sintomas}</span>
+				</p>
+
+				<div className="flex justify-between mt-10">
+					<button
+						type="button"
+						className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text 
+						text-white font-bold uppercase rounded-lg"
+						onClick={() => setPaciente(paciente)}
+					>
+						Editar
+					</button>
+					<button
+						type="button"
+						className="py-2 px-8 bg-red-600 hover:bg-red-700 
+						text-white font-bold uppercase rounded-lg"
+						onClick={handleEliminar}
+					>
+						Eliminar
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Paciente;
